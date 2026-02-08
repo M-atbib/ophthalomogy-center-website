66c055416f2d96584015b2d09796c0c33236aa3e
M-atbib <mokhtaratbib567@gmail.com>
2026-02-08 20:20:37 +0100

    saving some changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,10 @@
 	import favicon from '$lib/assets/logo.png';
 	import { Footer, Navbar } from '$lib';
 	import QuickAction from '$lib/components/general/QuickAction.svelte';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { dev } from '$app/environment';
 
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 	let { children } = $props();
 </script>
 
@@ -11,7 +14,7 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="bg-primary-background text-primary flex min-h-screen flex-col">
+<div class="flex min-h-screen flex-col bg-primary-background text-primary">
 	<Navbar />
 
 	<main class="w-full flex-1">
